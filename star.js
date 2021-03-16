1758f7efd9e7842d5bd561274687059348a9d111
Tatiana <tatiana@140-113-178-42.Dorm-F2.NCTU.edu.tw>
2021-03-16 18:29:55 +0800

    第四週練習

---
+++ star.js
@@ -0,0 +1,47 @@
+
+// var name ="Tatiana";
+// alert("Hello" + name);
+// var age = 19;
+// var isGirl = true;
+// var fruits = ["apple", "banana", "coconut"];
+
+// alert(name);
+// alert(age);
+// alert(isGirl);
+// alert(fruits[2]);
+
+// function addition(num){
+// 	return num+ 10;
+// }
+// var age = 28;
+// var trueage = addition(age);
+// alert(trueage);//"28"
+
+// var age = 15;
+// var trueage = addition(age);
+// alert(trueage);//"28"
+
+// function test() {
+
+// a =123;
+// var b=456;
+// if(a==123) 
+//    var c=789
+
+// }
+
+document.getElementById("byid").innerHTML="我來自id";
+document.getElementById("byid").style.color="blue";
+
+document.getElementsByClassName("byClass")[0].innerHTML="我來自class";
+document.getElementsByClassName("byClass")[0].style.fontSize="30px";
+
+document.getElementsByTagName("div")[2].innerHTML="我來自Tag";
+document.getElementsByTagName("div")[2].style.fontFamily="微軟正黑體";
+
+document.getElementsByTagName("p")[0].innerHTML="窗外的雲不是雲";
+document.getElementsByTagName("p")[0].style.color="red";
+document.getElementsByTagName("p")[0].style.fontSize="30px";
+
+
+
